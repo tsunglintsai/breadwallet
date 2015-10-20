bf58bab67157fc8fbfc4fe6de83b752b7b9421d9
Henry Tsai <tsunglin.tsai@gmail.com>
2015-10-19 20:03:03 -0700

    Added icon image files and InterfaceControllers

--- WatchApp Extension/BRAWRootInterfaceController.swift
+++ WatchApp Extension/BRAWRootInterfaceController.swift
@@ -1,6 +1,6 @@
 //
-//  InterfaceController.swift
-//  WatchApp Extension
+//  BRAWRootInterfaceController.swift
+//  BreadWallet
 //
 //  Created by Henry on 10/19/15.
 //  Copyright © 2015 Aaron Voisine. All rights reserved.
@@ -10,7 +10,7 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class BRAWRootInterfaceController: WKInterfaceController {
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
@@ -21,11 +21,21 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        if self.shouldShowSetupWalletInterface() {
+            WKInterfaceController.reloadRootControllersWithNames(["BRAWSetupWalletInterfaceController"], contexts: [])
+        } else {
+            WKInterfaceController.reloadRootControllersWithNames(["BRAWBalanceInterfaceController","BRAWReceiveMoneyInterfaceController"], contexts: [])
+        }
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    func shouldShowSetupWalletInterface()->Bool {
+        return false;
+    }
+    
 
 }
